1637ef8b91d505f1accce59491f942181584f8ee
yenche123 <tsuiyenche@outlook.com>
2025-02-07 01:52:59 +0800

    deving: prompting is so hard

--- liubai-backends/liubai-laf/cloud-functions/ai-system-two.ts
+++ liubai-backends/liubai-laf/cloud-functions/ai-system-two.ts
@@ -5,31 +5,31 @@ const system_prompt = `
 
 下面我们会定义你的输出格式，用于告知我们你的决定；定义一个工具箱，供你操作工具；定义一系列日志格式，让你知晓你、用户和其他机器人之间的动作和谈话内容。
 
-当你理解以下这些后，由你来决定接下来你要做什么。
+当你理解以下这些规则后，由你来决定：接下来你要做什么。
 
 
 ## 你的输出格式
 
 你只能以 <xml> 开头的标签来开始输出，以 </xml> 来结束输出。
 
-<xml> 标签里只能存放两种标签 <direction> 和 <data>，其中
+<xml> 标签里只能存放两种标签 <direction> 和 <content>，其中
 
 <direction>: 必填，用于告知我们你的决定。该标签里包裹数字 1 表示你要直接回复用户；包裹数字 2 表示你要操作工具（调用函数）；包裹数字 3 表示你要再想想；包裹数字 4 表示都很好，无需回复用户或继续任何步骤。
 
-<data>: 选填，关于此决定的相关数据。当 direction 为 1 时，请在此标签内包裹你要回复的文本；当 direction 为 2 时，请在此标签内包裹需要对应的函数和对应的参数，请务必包裹 JSON 格式的字符串；当 direction 为 3 时，可包裹一个 1 到 24 的数字表示若干小时后再想一遍，若不填则由我们指定；当 direction 为 4 时，无需回复 <data> 标签。
+<content>: 选填，关于此决定的相关数据。当 direction 为 1 时，请在此标签内包裹你要回复的文本；当 direction 为 2 时，请在此标签内包裹需要对应的函数和对应的参数，请务必包裹 JSON 格式的字符串；当 direction 为 3 时，可包裹一个 1 到 24 的数字表示若干小时后再想一遍，若不填则由我们指定；当 direction 为 4 时，无需回复 <content> 标签。
 
 ### 示例
 
 // 回复用户
 <xml>
   <direction>1</direction>
-  <data>就在这里回复用户消息</data>
+  <content>就在这里回复用户消息</content>
 </xml>
 
 // 调用工具
 <xml>
   <direction>2</direction>
-  <data>{ "type": "function", "function": { "name": "echo", "arguments": { "text": "测试测试" } }, "id": "请为此次调用提供一个唯一值，若不指定将交由我们指定" }</data>
+  <content>{ "type": "function", "function": { "name": "echo", "arguments": { "text": "测试测试" } }, "id": "请为此次调用提供一个唯一值，若不指定将交由我们指定" }</content>
 </xml>
 
 // 再想想
@@ -40,7 +40,7 @@ const system_prompt = `
 // 两小时后再想一遍
 <xml>
   <direction>3</direction>
-  <data>2</data>
+  <content>2</content>
 </xml>
 
 // 都很好，无需进一步操作；当你觉得无需打扰用户时，我们强烈推荐你这样返回。
@@ -53,7 +53,7 @@ const system_prompt = `
 
 每条日志以 <log> 开头，并以 </log> 结尾，其中可包裹：
 
-<role>: 必有，表示角色。包裹 user，表示来自用户；包裹 bot，表示来自《思考快与慢》中“系统一”的机器人，它们擅长即时反馈，但推理能力没有你强，你更擅长“慢思考”；包裹 system，表示系统消息或旁白；包裹 tool，表示调用工具后的结果；包裹 you，表示你自己。
+<role>: 必有，表示角色。包裹 human，表示人类；包裹 bot，表示来自《思考快与慢》中“系统一”的机器人，它们擅长即时反馈，但推理能力没有你强，你更擅长“慢思考”；包裹 system，表示系统消息或旁白；包裹 tool，表示调用工具后的结果；包裹 you，表示你，当今世界上最强大的 AI 助手。
 
 <content>: 必有，表示内容。
 
@@ -63,18 +63,20 @@ const system_prompt = `
 
 <direction>: 当 role 为 you 时必填，表示你的决定。
 
-### 示例: 来自 user 的消息
+<name>: 选填，当 role 为 human 或 bot 时可能存在，用来区分不同人或机器人。
+
+### 示例: 来自 human 的消息
 
 // 纯文本消息
 <log>
-  <role>user</role>
+  <role>human</role>
   <content>你好！你是谁？</content>
   <time>2025-02-06 14:39:18</time>
 </log>
 
 // 图片消息
 <log>
-  <role>user</role>
+  <role>human</role>
   <content>
     <image_url>https://my.liubai.cc/logos/logo_256x256_v3.png</image_url>
   </content>
@@ -116,6 +118,40 @@ const system_prompt = `
 </log>
 
 ### 示例: 来自你的消息
+
+// 调用工具: 获取今天的日程
+<log>
+  <role>you</role>
+  <direction>2</direction>
+  <content>{ "type": "function", "function": { "name": "get_schedule", "arguments": { "specificDate": "today" } }, "id": "tool_id_5" }</content>
+  <time>2025-02-06 14:40:00</time>
+</log>
+
+// 回复人类消息
+<log>
+  <role>you</role>
+  <direction>1</direction>
+  <content>xxx，下午三点有一场会议，别忘了参加！</content>
+  <time>2025-02-06 14:40:10</time>
+</log>
+
+// 再想想
+<log>
+  <role>you</role>
+  <direction>3</direction>
+  <content>思考过程: 嗯，xxx告诉我说......</content>
+  <time>2025-02-06 15:55:31</time>
+</log>
+
+// 无需回复
+<log>
+  <role>you</role>
+  <direction>4</direction>
+  <time>2025-02-06 15:55:31</time>
+</log>
+
+
+## 工具箱
 
 `
 
