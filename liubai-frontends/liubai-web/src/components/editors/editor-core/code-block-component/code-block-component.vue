4261a10d185328c5cfeb0622e4be5f6fa99fb251
yenche123 <tsuiyenche@outlook.com>
2025-04-25 14:57:34 +0800

    style: visualize button

--- liubai-frontends/liubai-web/src/components/editors/editor-core/code-block-component/code-block-component.vue
+++ liubai-frontends/liubai-web/src/components/editors/editor-core/code-block-component/code-block-component.vue
@@ -13,7 +13,7 @@
         <span>{{ t("editor.leave_codeBlock", { tip: leaveTip }) }}</span>
       </div>
 
-      <select contenteditable="false" v-model="showLanguage">
+      <select contenteditable="false" v-model="langDisplayed">
         <option :value="null">
           Auto
         </option>
@@ -29,11 +29,24 @@
 
     <!-- 阅读模式 -->
     <div v-else class="cb-right-top_read">
-      <div class="liu-no-user-select cbrt-tip">
-        <span v-if="showLanguage">{{ showLanguage }}</span>
+
+      <!-- Language, for example, TypeScript -->
+      <div v-if="!showVisualize" class="liu-no-user-select cbrt-tip">
+        <span v-if="langDisplayed">{{ langDisplayed }}</span>
         <span v-else>Auto</span>
       </div>
-      <div v-if="canInteract" class="cbrt-line" />
+      <div v-if="!showVisualize && canInteract" class="cbrt-line" />
+
+      <!-- Visualize -->
+      <div v-if="showVisualize && canInteract" 
+        class="liu-hover cbrt-tip cbrt-visualize"
+        @click.stop="onTapVisualize"
+      >
+        <span>{{ t('editor.one_click') }}</span>
+      </div>
+      <div v-if="showVisualize && canInteract" class="cbrt-line" />
+
+      <!-- Copy Button -->
       <div v-if="canInteract"
         class="cbrt-btn" 
         :class="{ 'cbrt-btn_no_pointer': showCopied }"
@@ -104,7 +117,7 @@ export default {
       return lang
     })
 
-    const showLanguage = computed({
+    const langDisplayed = computed({
       get: () => {
         const s = selectedLanguage.value
         if(!s) return s
@@ -117,13 +130,23 @@ export default {
       }
     })
 
+    const showVisualize = computed(() => {
+      const lang = langDisplayed.value
+      return Boolean(lang === "HTML")
+    })
 
     let copiedTimeout: LiuTimeout
     const showCopied = ref(false)
-    const onTapCopyCode = () => {
+
+    const _getCodePlainText = () => {
       //@ts-ignore
       const c = liuUtil.toRawData(props.node.content) as CbcFragment
       const text = c?.content?.[0].text
+      return text
+    }
+
+    const onTapCopyCode = () => {
+      const text = _getCodePlainText()
       if(!text) return
       liuApi.copyToClipboard(text)
       showCopied.value = true
@@ -132,6 +155,11 @@ export default {
         copiedTimeout = undefined
         showCopied.value = false
       }, 2000)
+    }
+
+    const onTapVisualize = () => {
+      const text = _getCodePlainText()
+      if(!text) return
     }
 
     const canInteract = inject(editorCanInteractKey, ref(true))
@@ -143,8 +171,10 @@ export default {
       isSafari,
       leaveTip, 
       selectedLanguage,
-      showLanguage,
+      langDisplayed,
+      showVisualize,
       onTapCopyCode,
+      onTapVisualize,
       showCopied,
       canInteract,
     }
@@ -219,7 +249,17 @@ export default {
       font-size: var(--mini-font);
       font-family: inherit;
       color: #606060;
-      margin-inline-end: 12px;
+      padding-inline: 6px;
+      margin-inline-end: 6px;
+    }
+
+    .cbrt-visualize {
+      --visualize-angle: 45deg;
+      background: linear-gradient(var(--visualize-angle), var(--inverse-primary), var(--primary-color) 70%);
+      background-clip: text;
+      -webkit-background-clip: text;
+      -webkit-text-fill-color: transparent;
+      color: transparent;
     }
 
     .cbrt-line {
@@ -254,6 +294,10 @@ export default {
     }
 
     @media(hover: hover) {
+      .cbrt-visualize:hover {
+        --visualize-angle: -15deg;
+      }
+
       .cbrt-btn:hover {
         background-color: #2f2f2f;
       }
